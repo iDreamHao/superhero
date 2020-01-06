9aad618d006d32d8384c5a459cced42f273c7bf2
dreamhao <idreamhao@gmail.com>
2020-01-07 00:15:48 +0800

    添加json和首页请求

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,8 +1,13 @@
 <template>
-	<view >
-		
-			<text class="title">{{title}}</text>
-		
+	<view class="page" >
+		<swiper :indicator-dots="true" :autoplay="true" class="carousel">
+			<swiper-item>
+				<image src="../../static/carousel/spiderman.png" class="carousel"></image>
+			</swiper-item>
+			<swiper-item>
+				<image src="../../static/carousel/batmanvssuperman.png" class="carousel"></image>
+			</swiper-item>
+		</swiper>
 	</view>
 </template>
 
@@ -10,11 +15,17 @@
 	export default {
 		data() {
 			return {
-				title: '首页'
+				
 			}
 		},
 		onLoad() {
-
+			uni.request({
+			    url: 'http://next-hero.oss-cn-shanghai.aliyuncs.com/json/index-carousel-list.json', 
+				method:'GET',
+			    success: (res) => {
+			        console.log(res.data);
+			    }
+			});
 		},
 		methods: {
 
@@ -23,5 +34,6 @@
 </script>
 
 <style>
+	@import url("index.css");
 
 </style>
