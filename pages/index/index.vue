8d71c87fbcd8dbea0349fdf37b17b587e00934c3
dreamhao <idreamhao@gmail.com>
2020-01-06 23:14:26 +0800

    初始化项目并添加底部tabbar

---
+++ pages/index/index.vue
@@ -0,0 +1,27 @@
+<template>
+	<view >
+		
+			<text class="title">{{title}}</text>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: '首页'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
